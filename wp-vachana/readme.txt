c311fa2097f7aa8687f74a792c2cf3a79135160b
Omshivaprakash <omshivaprakash@86088.no-reply.drupal.org>
2014-03-20 23:51:53 +0530

    Initial commit

---
+++ wp-vachana/readme.txt
@@ -0,0 +1,44 @@
+=== Vachana Sanchaya Daily Vachana Widget ===
+Contributors: Omshivaprakash
+Website link: http://www.linuxaayana.net
+License: GPLv3
+License URI: http://www.gnu.org/copyleft/gpl.html
+Tags: kannada, unicode, literature, vachana, sahitya, widget
+Requires at least: 2.9
+Tested up to: 3.8.1
+Stable tag: 1.0
+
+Display daily vachana from http://vachana.sanchaya.net
+
+== Description ==
+Vachana Sanchaya is a website dedicated for linquistic research on 11th and 12th Century literature called "Vachana Sahitya". Provides access to 21000 Vachana's (verses of poems) written by 258 Vachanakaara's (Poets/Creators of those Vachanas) in Unicode.  This widget is disgned to display the daily vachana from the site vachana.sanchaya.net to wordpress blogs. 
+
+Visit <a href="http://vachana.sanchaya.net/">Vachana Sanchaya</a> for more information. Visit <a href="http://en.wikipedia.org/wiki/Vachana sahitya">Vachana Sahitya</a> page on Wikipedia for more information on Vachana Sahitya movement.
+
+== Installation ==
+
+1. Upload the `vachana` folder to the `/wp-content/plugins/` directory
+2. Activate the Vachana Sanchaya plugin through the 'Plugins' menu in WordPress
+3. Go to Appearance --> Widgets to add the Vachana Sanchaya Daily Vachana widget to any widgetized areas, such as sidebars.
+
+== Screenshots ==
+
+1. Vachana Sanchaya Daily Vachana widget showing a daily vachana
+2. Vachana Sanchaya Daily Vachana in Installed Plugins page
+2. Vachana Sanchaya Daily Vachana in Appearance > Widgets
+3. Vachana Sanchaya Daily Vachana Widget when added to sidebar. Change it's title if you like.
+
+== Frequently Asked Questions ==
+
+= How to style the widget? =
+The vachana output wrapped in `<p>` tag with `vachana` ID and class. Use `#vachana` or `.vachana` in your Custom CSS to style it.
+
+= How can I contact you? =
+Use Linuxaayan's website's  <a href="http://linuxaayana.net/contact-us/">contact form</a> to reach me. You can also use Vachana Sanchaya group to discuss further on other developments <a href="http://vachana.sanchaya.net/contact_us">here</a>. 
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial public release.
+
---
+++ wp-vachana/vachana.php
@@ -0,0 +1,141 @@
+<?php
+/*
+Plugin Name: Vachana Sanchaya Daily Vachana
+Plugin URI: Plugin link: http://www.linuxaayana.net/2014/03/%E0%B2%B5%E0%B2%9A%E0%B2%A8-%E0%B2%B8%E0%B2%82%E0%B2%9A%E0%B2%AF-%E0%B2%B5%E0%B2%B0%E0%B3%8D%E0%B2%A1%E0%B3%8D%E2%80%8C%E0%B2%AA%E0%B3%8D%E0%B2%B0%E0%B3%86%E0%B2%B8%E0%B3%8D-%E0%B2%AA%E0%B3%8D/
+Description: This widget is disgned to display the daily vachana from the site vachana.sanchaya.net to wordpress blogs. 
+Author: Omshivaprakash H L
+Version: 1.0
+Author URI: http://www.linuxaayana.net
+*/
+
+/*  This file is part of Vachana Sanchaya Daily Vachana plugin, developed by Omshivaprakash H L (email: naan@shivu.in)
+
+    Vachana sahitya (Kannada: ವಚನ ಸಾಹಿತ್ಯ) is a form of rhythmic writing in Kannada 
+    that evolved in the 11th Century C.E. and flourished in the 12th century, as a part of the Lingayatha 'movement'. 
+    Vachana Sanchaya is a website dedicated for linquistic research on "Vachana Sahitya". It provides access to 
+    21000 Vachana's (verses of poems) written by 258 Vachanakaara's (Poets/Creators of those Vachanas) in Unicode. 
+
+    Vachana Sanchaya Daily Vachana Plugin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Vachana Sanchaya Daily Vachana Plugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mankutimmana Kagga plugin. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+function todays_vachana() {
+	 $url_send='http://vachana.sanchaya.net/api/today_vachana';
+                        $curl = curl_init();
+                         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                         curl_setopt($curl, CURLOPT_POST, false);
+                         curl_setopt($curl, CURLOPT_URL, $url_send);
+                         curl_setopt($curl, CURLOPT_HEADER, false);
+                         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+                        $response = curl_exec($curl);
+                        curl_close($curl);
+                        $decoded_response=json_decode($response);
+
+        $vachana_info = explode("\n", $decoded_response->vachana->vachana);
+        $display = null;
+        foreach ($vachana_info as $key=>$value)
+                {
+                        $display .= $value."<br />";
+                }
+        $display .=  ": <b>". $decoded_response->vachana->vachanakaara->name."</b>";
+
+	// Display Vachana
+	return $display;
+}
+
+function show_vachana() {
+	$vachanas = todays_vachana();
+	echo "<p id='vachana' class='vachana'>$vachanas</p>";
+}
+
+class Vachana_Sanchaya_Widget extends WP_Widget {
+			
+	function __construct() {
+    	$widget_ops = array(
+			'classname'   => 'vachana_sanchaya_widget', 
+			'description' => __('Show todays\'s Vachana from Vachana Sanchaya Website. ವಚನ ಸಂಚಯ ವೆಬ್‌ಸೈಟ್‌ನಿಂದ ಇಂದಿನ ವಚನ ತೋರಿಸಿ.')
+		);
+    	parent::__construct('vachana-sanchaya-vachana', __('Vachana Sanchaya Daily Vachana'), $widget_ops);
+	}
+	
+	function widget($args, $instance) {
+           
+			extract( $args );
+		
+			$title = apply_filters( 'widget_title', empty($instance['title']) ? 'ವಚನ ಸಂಚಯ: ಇಂದಿನ ವಚನ' : $instance['title'], $instance, $this->id_base);	
+			
+			echo $before_widget;
+						
+			// Widget title
+			
+			echo $before_title;
+			
+			echo $instance["title"];
+			
+			echo $after_title;
+						
+			// Call show_vachana function
+			
+    			show_vachana();
+
+		echo $after_widget;
+
+	}
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+	     
+        		return $instance;
+	}
+	
+	
+	function form( $instance ) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : 'ವಚನ ಸಂಚಯ: ಇಂದಿನ ವಚನ ';
+		
+?>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+             
+<?php
+	}
+}
+
+function mtk_register_widgets() {
+	register_widget( 'Vachana_Sanchaya_Widget' );
+}
+
+add_action( 'widgets_init', 'mtk_register_widgets' );
+
+// Donate link on manage plugin page
+function mtk_pluginspage_links( $links, $file ) {
+
+$plugin = plugin_basename(__FILE__);
+
+// create links
+if ( $file == $plugin ) {
+return array_merge(
+$links,
+array( '<a href="http://vachana.sanchaya.net" target="_blank" title="Vachana Sanchaya Website">Vachana Sanchaya</a>',
+'<a href="http://linuxaayana.net" target="_blank" title="View more FOSS plugins for Kannada">Linuxaayana</a>',
+'<a href="http://twitter.com/omshivaprakash" target="_blank" title="Follow Omshivaprakash!">Follow on Twitter</a>'
+ )
+);
+			}
+return $links;
+
+	}
+add_filter( 'plugin_row_meta', 'mtk_pluginspage_links', 10, 2 );
+?>
